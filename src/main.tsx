8604b54e9496d89378aece3c5e011122d79e0e49
sai <sai.kumar.mediboina@oracle.com>
2026-09-11 19:08:19 +0530

    Redesign active builds and assistant pages with shared editorial theme

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import "./scroll-header.css";
 import "./site-design.css";
 import "./blog-journal.css";
 import "./personal-pages.css";
+import "./build-pages.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
